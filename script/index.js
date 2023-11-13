5e2087b214f71a2b383c1b3c2cfba80e0d25b70d
Wareezy <warrenjaftha16@gmail.com>
2023-11-13 13:39:20 +0200

    changes2

--- script/index.js
+++ script/index.js
@@ -1,35 +1,16 @@
-//  let btn=document.querySelector('[data-calculate]')
-//  btn.addEventListener('click', calculate)
-//  let display=document.querySelector('[data-answer]')
-
-
-
-//  function calculate()
-//  {
-//     let height=document.querySelector('[data-height]').value
-//     let weight=document.querySelector('[data-weight]').value
-//     let answer= eval(` ${weight}/Math.pow(${height},2) `)
-//     display.innerHTML=answer
-//  }
 
 let btn = document.querySelector('[data-calculate]');
 btn.addEventListener('click', calculate);
 let display = document.querySelector('[data-answer]');
 
 function calculate() {
-// Get the values from the input fields
 let heightInCentimeters = parseFloat(document.querySelector('[data-height]').value);
-    let weight = parseFloat(document.querySelector('[data-weight]').value);
-    // Convert height to meters (divide by 100 as 1 meter = 100 centimeters)
-    let heightInMeters = heightInCentimeters / 100;
-    // Check if the inputs are valid numbers and positive
-    if (!isNaN(heightInMeters) && !isNaN(weight) && heightInMeters > 0 && weight > 0) {
-        // Calculate BMI using the formula: BMI = weight / (height * height)
-        let bmi = weight / Math.pow(heightInMeters, 2);
-        // Display the result
-        display.innerHTML = 'Your BMI: ' + bmi.toFixed(2);
-    } else {
-        // Display an error message if inputs are invalid
+let weight = parseFloat(document.querySelector('[data-weight]').value);
+let heightInMeters = heightInCentimeters / 100;
+if (!isNaN(heightInMeters) && !isNaN(weight) && heightInMeters > 0 && weight > 0) {
+let bmi = weight / Math.pow(heightInMeters, 2);
+display.innerHTML = 'Your BMI: ' + bmi.toFixed(2); } 
+    else {
         display.innerHTML = 'Please enter valid height and weight.';
     }
 }
@@ -48,21 +29,3 @@ let heightInCentimeters = parseFloat(document.querySelector('[data-height]').val
 
 
 
-
-// let btn = document.querySelector('[data-calculate]');
-// btn.addEventListener('click', calculate);
-
-// let display = document.querySelector('[data-answer]');
-
-// function calculate() {
-//     let height = parseFloat(document.querySelector('[data-height]').value);
-//     let weight = parseFloat(document.querySelector('[data-weight]').value);
-
-//     if (!isNaN(height) && !isNaN(weight) && height > 0 && weight > 0) {
-//         // Calculate BMI using the formula: BMI = weight / (height * height)
-//         let bmi = weight / Math.pow(height, 2);
-//         display.innerHTML = 'Your BMI: ' + bmi.toFixed(2);
-//     } else {
-//         display.innerHTML = 'Please enter valid height and weight.';
-//     }
-// }
